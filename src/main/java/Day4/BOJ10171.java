5c8b3e7e1cf0ef0105e809849b0027270d1570dc
rami <sbr95887208@gmail.com>
2023-11-13 15:52:58 +0900

    day4 commit

---
+++ src/main/java/Day4/BOJ10171.java
@@ -0,0 +1,14 @@
+package Day4;
+
+public class BOJ10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+
+// 이스케이프 문자 처리를 위해서 앞에 역슬레시를 붙여줘야 한다.
+// System.out.println("\"안녕\"");  -> "안녕"
+// System.out.println("\\n") -> \n
