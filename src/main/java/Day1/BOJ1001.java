d475aaff6f2745f93a466e94763d8b9d6d66eb21
rami <sbr95887208@gmail.com>
2023-11-13 11:12:39 +0900

    day1 commit

--- src/main/java/Day1/BOJ1001.java
+++ src/main/java/Day1/BOJ1001.java
@@ -2,13 +2,11 @@ package Day1;
 
 import java.util.Scanner;
 
-public class BOJ1000 {
+public class BOJ1001 {
     public static void main(String[] args) {
-        // 입력값 받을 때는 Scanner 또는 BufferedReader 사용
-        // 입력값 많을 때는 BufferedReader 쓰는게 좋음
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
-        System.out.println(A+B);
+        System.out.println(A-B);
     }
 }
---
+++ src/main/java/Day1/BOJ10718.java
@@ -0,0 +1,7 @@
+package Day1;
+
+public class BOJ10718 {
+    public static void main(String[] args) {
+        System.out.print("강한친구 대한육군\n강한친구 대한육군");
+    }
+}
