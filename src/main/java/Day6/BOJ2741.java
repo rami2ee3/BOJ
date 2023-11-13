6843ff1a264ce5ddf9b000d56d2fc7d2ee17b620
rami <sbr95887208@gmail.com>
2023-11-13 16:53:57 +0900

    day6 commit

---
+++ src/main/java/Day6/BOJ2741.java
@@ -0,0 +1,17 @@
+package Day6;
+
+import java.util.Scanner;
+
+public class BOJ2741 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i = 1; i <= N; i++){
+            System.out.println(i);
+        }
+    }
+}
+
+// 문제) 시간 제한 1초. 100,000보다 작거나 같은 자연수 N
+// 자바에서 일억번 반복 -> 1초 걸림
+// for문 10만번 도는건 1초 내에 해결 가능
