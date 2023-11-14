36cc1fd931fcdeefba5bcae9c7be4e62bdc756c5
rami <sbr95887208@gmail.com>
2023-11-14 14:38:29 +0900

    day10 commit

---
+++ src/main/java/Bronze5/Day10/BOJ11654.java
@@ -0,0 +1,17 @@
+package Bronze5.Day10;
+
+import java.util.Scanner;
+
+public class BOJ11654 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0); //0번째
+        System.out.println((int)ch);
+
+        //  숫자 -> 문자
+//        int num = sc.nextInt();
+//        System.out.println((char)num);
+    }
+}
+
