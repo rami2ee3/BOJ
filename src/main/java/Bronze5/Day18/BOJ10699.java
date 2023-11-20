a75fa9f44c707bfa7ea2e5c505583d4e1c7023bd
rami <sbr95887208@gmail.com>
2023-11-20 16:43:45 +0900

    day18 commit

---
+++ src/main/java/Bronze5/Day18/BOJ10699.java
@@ -0,0 +1,12 @@
+package Bronze5.Day18;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class BOJ10699 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY-MM-dd");
+        System.out.println(simpleDateFormat.format(date));
+    }
+}
