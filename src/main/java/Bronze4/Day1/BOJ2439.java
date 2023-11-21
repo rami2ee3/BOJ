f9beb9c1d6c457f0d319fb79f14310269f5bb5a4
rami <sbr95887208@gmail.com>
2023-11-21 10:44:21 +0900

    Bronze4 day2 commit

--- src/main/java/Bronze4/Day1/BOJ2439.java
+++ src/main/java/Bronze4/Day1/BOJ2439.java
@@ -6,8 +6,8 @@ public class BOJ2439 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        for(int i = N; i > 0; i--){
-            for(int j = 1; j <= N; j++){
+        for(int i = N; i > 0; i--){       //5 4 3 2 1
+            for(int j = 1; j <= N; j++){  //1 2 3 4 5
                 if(i <= j){
                     System.out.print("*");
                 }else{
---
+++ src/main/java/Bronze4/Day3/BOJ2480.java
@@ -0,0 +1,7 @@
+package Bronze4.Day3;
+
+public class BOJ2480 {
+    public static void main(String[] args) {
+
+    }
+}
