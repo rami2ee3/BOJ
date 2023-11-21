bac9dafeb7884aa38376eed5a518c6e0842c18d3
rami <sbr95887208@gmail.com>
2023-11-21 10:04:43 +0900

    Bronze4 day1 commit

---
+++ src/main/java/Bronze4/Day1/BOJ2742.java
@@ -0,0 +1,13 @@
+package Bronze4.Day1;
+
+import java.util.Scanner;
+
+public class BOJ2742 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=N; i>0; i--){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/Bronze4/Day2/BOJ15552.java
@@ -0,0 +1,7 @@
+package Bronze4.Day2;
+
+public class BOJ15552 {
+    public static void main(String[] args) {
+
+    }
+}
