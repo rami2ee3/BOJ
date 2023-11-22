0c17b22e3084a158d1dd34e0d7053164e9249703
rami <sbr95887208@gmail.com>
2023-11-22 13:59:57 +0900

    Bronze4 day7 commit

---
+++ src/main/java/Bronze4/Day7/BOJ2752.java
@@ -0,0 +1,29 @@
+package Bronze4.Day7;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BOJ2752 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int[] arr = new int[3];
+//        arr[0] = sc.nextInt();
+//        arr[1] = sc.nextInt();
+//        arr[2] = sc.nextInt();
+//        Arrays.sort(arr); //오름차순
+//        System.out.println(arr[2]+" "+arr[1]+" "+arr[0]); //내림차순
+//    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer[] arr = new Integer[3];
+        arr[0] = sc.nextInt();
+        arr[1] = sc.nextInt();
+        arr[2] = sc.nextInt();
+        Arrays.sort(arr, Collections.reverseOrder()); //내림차순
+        System.out.println(arr[0]+" "+arr[1]+" "+arr[2]);
+    }
+
+
+}
