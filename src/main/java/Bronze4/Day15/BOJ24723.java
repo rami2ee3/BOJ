5da5b773bcf01dd4f8411f13fe3789cbcad3539e
rami <sbr95887208@gmail.com>
2023-12-15 15:12:48 +0900

    Bronze4 day15 commit

---
+++ src/main/java/Bronze4/Day15/BOJ24723.java
@@ -0,0 +1,11 @@
+package Bronze4.Day15;
+
+import java.util.Scanner;
+
+public class BOJ24723 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println((int)Math.pow(2, N));
+    }
+}
